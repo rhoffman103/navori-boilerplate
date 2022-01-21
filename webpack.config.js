2ba7fa6ec7f6d981ecb9db8a7db7c41a693b8029
Bobby H <rhoffman103@gmail.com>
2022-01-20 23:38:41 -0500

    feat(webpack): styles config
    basic css loaders. import css via js file or add css entry files

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,42 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const mode = process.env.NODE_ENV || "development";
+const isProd = mode === "production";
 
 const config = {
   mode: "development",
   entry: {
     index: {
-      import: ["./src/js/index.js"]
+      import: ["./src/js/index.js", "./src/styles/index.css"]
     }
   },
   plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
     new HtmlWebpackPlugin({
       inject: true,
       template: path.resolve(__dirname, "src", "index.html")
     })
   ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          isProd ? MiniCssExtractPlugin.loader : "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true
+            }
+          }
+        ],
+      }
+    ]
+  },
   output: {
     filename: "[name].[contenthash].bundle.js",
     path: path.resolve(__dirname, "dist"),
@@ -40,7 +61,7 @@ const config = {
   },
 };
 
-if (mode === "development") {
+if (!isProd) {
   config.devtool = "inline-source-map"
 }
 
