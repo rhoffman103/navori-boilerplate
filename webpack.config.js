6d4ee2e9a64b0bd3b34f2141596d281f8698ae61
Bobby H <rhoffman103@gmail.com>
2022-01-20 22:44:56 -0500

    feat(webpack): further develop config
    change src sile architecture
    
    add webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -7,32 +7,32 @@ const config = {
   mode: "development",
   entry: {
     index: {
-      import: ["./src/index.js"]
+      import: ["./src/js/index.js"]
     }
   },
   plugins: [
     new HtmlWebpackPlugin({
       inject: true,
-      template: path.resolve(__dirname, "public", "index.html")
+      template: path.resolve(__dirname, "src", "index.html")
     })
   ],
   output: {
-    filename: '[name].[contenthash].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "[name].[contenthash].bundle.js",
+    path: path.resolve(__dirname, "dist"),
     clean: true,
   },
   optimization: {
     splitChunks: {
-      chunks: 'all',
+      chunks: "all",
     },
-    runtimeChunk: 'single',
-    moduleIds: 'deterministic',
+    runtimeChunk: "single",
+    moduleIds: "deterministic",
     splitChunks: {
       cacheGroups: {
         vendor: {
           test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all',
+          name: "vendors",
+          chunks: "all",
         },
       },
     },
@@ -40,11 +40,8 @@ const config = {
   },
 };
 
-if (mode === 'development') {
-  config.devtool = 'inline-source-map'
-  config.devServer = {
-    static: './dist',
-  };
+if (mode === "development") {
+  config.devtool = "inline-source-map"
 }
 
 module.exports = config;
