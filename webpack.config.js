1a21d7defd7b5ffed95fd292d3f47a98a817e088
Bobby H <rhoffman103@gmail.com>
2022-01-22 00:33:57 -0500

    feat(webpack): babel-preset for js

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,16 @@ const config = {
   module: {
     rules: [
       {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
         test: /\.css$/i,
         use: [
           isProd ? MiniCssExtractPlugin.loader : "style-loader",
