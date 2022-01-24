92e7c2bebe5b257b9d821b75940e1af47acb2757
Bobby H <rhoffman103@gmail.com>
2022-01-23 20:59:38 -0500

    feat(config): image & font loader

--- webpack.config.js
+++ webpack.config.js
@@ -52,7 +52,15 @@ const config = {
             }
           }
         ],
-      }
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
     ]
   },
   resolve: {
